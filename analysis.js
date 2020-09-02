7e9075d167bdb9860954142e4e9767ce5f31cfab
Vitor Lima <vitorfdl@tago.io>
2020-09-02 10:10:31 -0300

    Update analysis.js

--- analysis.js
+++ analysis.js
@@ -1,119 +1,91 @@
 /*
- * Analysis Example
- * Geofences
- *
- * Use geofences to control the area that your devices are in.
- *
- * Instructions
- * To run this analysis you need to add an account token to the environment variables,
- * To do that, go to your account settings, then token and copy your token.
- * 1 - Enter the following link: https://admin.tago.io/account/
- * 2 - Select your Profile.
- * 3 - Enter Tokens tab.
- * 4 - Generate a new Token with Expires Never.
- * 5 - Press the Copy Button and place at the Environment Variables tab of this analysis with key account_token.
- *
- * Follow this guide https://docs.tago.io/en/articles/151 and create
- * two geofences, one with the event code 'danger' and another named 'safe'.
+ ** Analysis Example
+ ** Get users total transactions
+ **
+ ** This analysis must run by an Scheduled Action.
+ ** It gets a total amount of transactions by device, calculating by the total amount of data in the bucket
+ ** each time the analysis run. Group the result by a tag.
+ **
+ ** Environment Variables
+ ** In order to use this analysis, you must setup the Environment Variable table.
+ **
+ ** device_token: Token of a device where the total transactions will be stored. Get this in the Device's page.
+ ** account_token: Your account token. Check bellow how to get this.
+ **
+ ** Steps to generate an account_token:
+ ** 1 - Enter the following link: https://admin.tago.io/account/
+ ** 2 - Select your Profile.
+ ** 3 - Enter Tokens tab.
+ ** 4 - Generate a new Token with Expires Never.
+ ** 5 - Press the Copy Button and place at the Environment Variables tab of this analysis.
  */
 
-const { Utils, Account, Analysis, Device, Services } = require("@tago-io/sdk");
-const geolib = require("geolib");
+const { Analysis, Account, Utils, Device } = require('@tago-io/sdk');
+const _ = require('lodash');
 
+async function calculateUserTransactions(account, storage, user_value, device_list) {
+  // Collect the data amount for each device.
+  // Result of bucket_results is:
+  // [0, 120, 500, 0, 1000]
+  const bucket_results = await Promise.all(device_list.map(device => account.buckets.amount(device.bucket)));
+  const total_transactions = _.sum(bucket_results);
 
-// This function checks if our device is inside a polygon geofence
-function insidePolygon(point, geofence) {
-  const x = point[1];
-  const y = point[0];
-  let inside = false;
-  for (let i = 0, j = geofence.length - 1; i < geofence.length; j = i++) {
-    const xi = geofence[i][0];
-    const yi = geofence[i][1];
-    const xj = geofence[j][0];
-    const yj = geofence[j][1];
-    const intersect = yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
-    if (intersect) inside = !inside;
+  // Get the total transactions of the last analysis run.
+  // Serie is used to get only for this user.
+  // You can change that to get a specific device for the user, instead of using a global storage device.
+  // One way to do that is by just finding the device using a tag, see example:
+  //
+  // const [user_device] = await account.devices.list({ page: 1, fields: ['id', 'name', 'bucket', 'tags'], filter: { tags: [{ key: 'user_device', value: user_value }] }, amount: 1 });
+  // const device_token = await Utils.getTokenByName(account, user_device.id);
+  // const storage = new Device({ token: device_token });
+  let [last_total_transactions] = await storage.getData({ variable: 'last_transactions', qty: 1, serie: user_value });
+  if (!last_total_transactions) {
+    last_total_transactions = { value: 0 };
   }
-  return inside;
+
+  const result = total_transactions - last_total_transactions.value;
+
+  // Store the current total of transactions, the result for this analysis run and the key.
+  // Now you can just plot these variables in a dynamic table.
+  await storage.sendData([
+    { variable: 'last_transactions', value: total_transactions, serie: user_value },
+    { variable: 'transactions_result', value: result, serie: user_value },
+    { variable: 'user', value: user_value, serie: user_value },
+  ]);
 }
 
-// This function checks if our device is inside any geofence
-function checkZones(point, geofence_list) {
-  // The line below gets all Polygon geofences that we may have.
-  const polygons = geofence_list.filter((x) => x.geolocation.type === "Polygon");
-  if (polygons.length) {
-    // Here we check if our device is inside any Polygon geofence using our function above.
-    const pass_check = polygons.map((x) => insidePolygon(point, x.geolocation.coordinates[0]));
-    const index = pass_check.findIndex((x) => x === true);
-    if (index !== -1) return polygons[index];
-  }
-  // The line below gets all Point (circle) geofences that we may have.
-  const circles = geofence_list.filter((x) => x.geolocation.type === "Point");
-  if (circles.length) {
-    // Here we check if our device is inside any Point geofence using a third party library called geolib.
-    const pass_check = circles.map((x) =>
-      geolib.isPointWithinRadius(
-        { latitude: point[1], longitude: point[0] },
-        { latitude: x.geolocation.coordinates[0], longitude: x.geolocation.coordinates[1] },
-        x.geolocation.radius
-      )
-    );
-    const index = pass_check.findIndex((x) => x);
-    if (index !== -1) return circles[index];
-  }
-  return;
-}
-
-// This function help us get the device using just its id.
-async function getDevice(account, device_id) {
-  const customer_token = await Utils.getTokenByName(account, device_id);
-  const customer_dev = new Device({ token: customer_token });
-  return customer_dev;
-}
-
-async function startAnalysis(context, scope) {
-  context.log("Running");
-
-  if (!scope[0]) throw "Scope is missing"; // doesn't need to run if scope[0] is null
-
-  // The code block below gets all environment variables and checks if we have the needed ones.
+async function myAnalysis(context) {
+  // Transform all Environment Variable to JSON.
   const environment = Utils.envToJson(context.environment);
-  if (!environment.account_token) throw "Missing account_token environment var";
-
+  if (!environment.account_token) {
+    return context.log('You must setup an account_token in the Environment Variables.');
+  } else if (!environment.device_token) {
+    return context.log('You must setup an device_token in the Environment Variables.');
+  }
+  // Instance the account class
   const account = new Account({ token: environment.account_token });
-  const device_id = scope[0].origin;
+  const storage = new Device({ token: environment.device_token });
 
-  // Here we get the device information using our account data and the device id.
-  const device = await getDevice(account, device_id);
+  // Setup the tag we will be searching in the device list
+  const tag_to_search = 'user_email';
 
-  // This checks if we received a location
-  const location = scope.find((data) => data.variable === "location");
-  if (!location || !location.location) return context.log("No location found in the scope.");
+  // Get the device_list and group it by the tag value.
+  // Result of grouped_device_list is:
+  // [
+  //   { value: 'test@tago.io', device_list: [ [Object], [Object] ] },
+  //   { value: 'user@tago.io', device_list: [ [Object] ] }
+  // ]
+  const device_list = await account.devices.list({ page: 1, fields: ['id', 'name', 'bucket', 'tags'], filter: { tags: [{ key: tag_to_search }] }, amount: 10000 });
+  const grouped_device_list = _.chain(device_list)
+                              .groupBy(collection => collection.tags.find(x => x.key === tag_to_search).value)
+                              .map((value, key) => ({ value: key, device_list: value }))
+                              .value();
 
-  // Now we check if we have any geofences to go through.
-  const geofences = await device.getData({ variable: "geofence", qty: 10 });
-  const zones = geofences.map((geofence) => geofence.metadata);
-
-  const zone = checkZones(location.location.coordinates, zones);
-  if (!zone) {
-    // If no geofence is found, we stop our application sending a notification.
-    notification.send({ title: "No zone alert", message: "Your device is not inside any zone." });
-    context.log("Your device is not inside any zone.");
-    return;
-  }
-
-  // The line below starts our notification service.
-  const notification = new Services({ token: context.token }).Notification;
-  if (zone.event === "danger") {
-    // If our device is inside a danger geofence, we will send a notification with a danger alert.
-    notification.send({ title: "Danger alert", message: "Your device is inside a dangerous zone." });
-  }
-  if (zone.event === "safe") {
-    // If our device is inside a safe geofence, we will send a safe geofence notification.
-    notification.send({ title: "Sage alert", message: "Your device is inside a safe zone." });
-  }
-
-  context.log(zone.event);
+  // Call a new function for each group in assynchronous way.
+  await Promise.all(grouped_device_list.map(group => calculateUserTransactions(account, storage, group.value, group.device_list)));
 }
 
-module.exports = new Analysis(startAnalysis);
+module.exports = new Analysis(myAnalysis, { token: 'f6482b87-0877-4fa3-8f01-ad62b2d9760e' });
+
+// To run analysis on your machine (external)
+// module.exports = new Analysis(myAnalysis, { token: "YOUR-TOKEN" });
